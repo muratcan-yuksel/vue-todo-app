1417c8fca5bd186dfb113d4f5bc73ad46422c63a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-10 16:13:31 +0300

    ..

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Vue 3 Todo App</h1>
+  <form action="" @submit.prevent="addNewTodo">
+    <label for="New Todo"></label>
+    <input v-model="newTodo" name="newTodo" type="text" />
+    <button>Add New Todo</button>
+  </form>
+  <h2>{{ newTodo }}</h2>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const newTodo = ref("");
+    function addNewTodo() {
+      console.log("submitted bro");
+      console.log(newTodo.value);
+    }
+    return {
+      addNewTodo,
+      newTodo,
+    };
+  },
+};
+</script>
+
+<style></style>
